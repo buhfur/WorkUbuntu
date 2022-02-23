26682c2822793b55693dcbea82039392a935c0b8
tvhead <ryanmcvicker12@gmail.com>
2022-02-23 04:35:53 -0500

    changes

--- setup.sh
+++ setup.sh
@@ -4,26 +4,26 @@
 # clone Journal repo  
 #sudo all files in ./files directory 
 sudo chmod +x ./files
-#create data directory just to hold all files and data that I might want to backup everytime I work 
-if [ -d "data"]; then
-	mkdir ~/data
+#create stuff directory just to hold all files and data that I might want to backup everytime I work 
+if [ ! -d "stuff"]; then
+	mkdir ~/stuff
 fi
 cat files/vimrc > ~/.vimrc
 git config --global user.email "ryanmcvicker12@gmail.com"
 git config --global user.name "tvhead"
 git config --global credential.helper store
 
-if [ ! -f ~/data/Journal ]; then
-	git clone https://github.com/ryanmcvicker12/Journal ~/data
+if [ ! -d ~/stuff/Journal ]; then
+	git clone https://github.com/ryanmcvicker12/Journal ~/stuff
 fi
 #update bashrc to one in ./files
 cat ./files/bashrc > ~/.bashrc
-#dont even really need to cat the file , could just source it since the machine doesn't save any data after shutdown 
+#dont even really need to cat the file , could just source it since the machine doesn't save any stuff after shutdown 
 #install pwngdb 
-if [ ! -f ~/data/pwndbg ]; then
-	git clone https://github.com/pwndbg/pwndbg ~/data
+if [ ! -d ~/stuff/pwndbg ]; then
+	git clone https://github.com/pwndbg/pwndbg 
+	cd pwndbg
+	./setup.sh
 fi
 
-cd ~/data/pwndbg
-./setup.sh
 
